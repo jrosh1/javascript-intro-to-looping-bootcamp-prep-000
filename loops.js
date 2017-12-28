d956fd8e3dac9324e692fdab900a4483d43db781
Jacob Roshgadol <jacobiroshgadol@gmail.com>
2017-12-28 16:34:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,11 @@
 function forLoop(array){
- 
   for(let i = 1; i < 26; i++){
     if (i === 1){
       array = ["I am 1 strange loop."]
     }
     else{
-      
+       array = [...array, "I am ${i} strange loops."]
     }
-    array = [...array, "I am ${i} strange loops."]
   }
   return array
 }
